b71e6a6deabb4e911da41842cd182fa8072b1514
tdude0175 <tdude0175@gmail.com>
2019-09-23 11:49:00 -0500

    graded 2019-09-23

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # python_debugging
-
+#### You did well and caught most of the errors but some got through 3/5 Score: 2/4
 All of the questions 1 through 10 has bugs in it. Fix each of the questions using the debugger and reading error messages.
--- question5.py
+++ question5.py
@@ -2,5 +2,5 @@
 # Create the list: squad = ["One", "Two", "Three", "Four", "Five"]. Print the list in reverse without using a list method.
 squad = ["One", "Two", "Three", "Four", "Five"]
 for index in range(len(squad)-1, -1, -1):
-    print(squad(index))
+    print(squad(index))  # you need to use square brackets here around index instead of parenthesis
     # //Error was code should have read for index in range it said index of range. Uodated to in range.
